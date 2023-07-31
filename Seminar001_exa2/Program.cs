d5d7801c3639b70d86fc09edce01053ae33505e0
UzenLe <Uzenkovlesha@mail.ru>
2023-07-31 10:41:33 +0300

    days of the week

---
+++ Seminar001_exa2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter num:");
+string strNum1 = Console.ReadLine();
+
+int num1 = int.Parse(strNum1);
+if (num1 == 1)
+{
+Console.WriteLine($"Monday");
+}
+else if (num1 == 2)
+{
+    Console.WriteLine($"Tuesday");
+}
+else if (num1 == 3)
+
+{
+    Console.WriteLine($"Wednesday");
+}
+else if (num1 == 4)
+{
+    Console.WriteLine($"Thursday");
+}
+else if (num1 == 5)
+{
+    Console.WriteLine($"Friday");
+}
+else if (num1 == 6)
+{
+    Console.WriteLine($"Saturday");
+}
+else if (num1 == 7)
+{
+    Console.WriteLine($"Sunday");
+}
