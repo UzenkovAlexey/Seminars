7842c8f20a041a405022655df6ca15ffae02824c
UzenLe <Uzenkovlesha@mail.ru>
2023-07-31 11:47:59 +0300

    Example 3

---
+++ Seminar001_exa3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number: ");
+
+String strNum = Console.ReadLine();
+
+int num = int.Parse(strNum);
+
+int num1 = num * -1;
+
+  while (num1 <= num)
+  {
+    Console.WriteLine(num1);
+    num1 = num1+1;
+  }
