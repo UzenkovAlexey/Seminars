cd7ab66b9fcf6438acd93cd95a4acc2a70a8a32d
UzenLe <Uzenkovlesha@mail.ru>
2023-07-31 10:17:59 +0300

    Square

---
+++ Seminar001_exa1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Enter num:");
+string strNum1 = Console.ReadLine();
+
+Console.WriteLine("Enter num:");
+string strNum2 = Console.ReadLine();
+
+int num1 = int.Parse(strNum1);
+int num2 = int.Parse(strNum2);
+
+int square  = num2 * num2;
+if (square == num1)
+{
+Console.WriteLine($"Yes, the number {num1} is the square of the number {num2}");
+}
+else
+{
+    Console.WriteLine($"number {num1} is not a square of {num2}");
+}
